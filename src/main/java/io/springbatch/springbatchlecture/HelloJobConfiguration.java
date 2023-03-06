6dc1333d61140cef19abca83139270a9ea9985ae
최광진 <38902367+arattha@users.noreply.github.com>
2023-03-06 17:38:25 +0900

    [2023-03-06]

--- src/main/java/io/springbatch/springbatchlecture/HelloJobConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/HelloJobConfiguration.java
@@ -16,12 +16,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class HelloJobConfiguration {
 
-    private final JobBuilderFactory jobBuilderFactory;
-    private final StepBuilderFactory stepBuilderFactory;
+    private final JobBuilderFactory jobBuilderFactory; // JOB 생성용 유틸
+    private final StepBuilderFactory stepBuilderFactory; // STEP 생성용 유틸
 
     @Bean
     public Job helloJob() {
-        return this.jobBuilderFactory.get("helloJob")
+        return this.jobBuilderFactory.get("helloJob")// JOB 생성
                 .start(helloStep1())
                 .next(helloStep2())
                 .build();
@@ -29,18 +29,21 @@ public class HelloJobConfiguration {
 
     @Bean
     public Step helloStep1() {
-        return stepBuilderFactory.get("helloStep1")
-                .tasklet(new Tasklet() {
+        return stepBuilderFactory.get("helloStep1") // STEP 생성
+                .tasklet(new Tasklet() { //기본적으로 무한 반복
                     @Override
                     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
                         System.out.println(" ============================");
                         System.out.println(" >> Hello Spring Batch");
                         System.out.println(" ============================");
-                        return RepeatStatus.FINISHED;
+                        return RepeatStatus.FINISHED; // null 과 동일, 1번 실행 후 종료
                     }
                 })
+                //tasklet 단일 태스크로 수행되는 로직 실행
                 .build();
     }
+
+    @Bean
     public Step helloStep2() {
         return stepBuilderFactory.get("helloStep2")
                 .tasklet((contribution, chunkContext) -> {
