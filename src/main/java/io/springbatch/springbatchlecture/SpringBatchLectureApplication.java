4130f3ce68c73e89446380183094ce6854fc0a2f
최광진 <38902367+arattha@users.noreply.github.com>
2023-03-06 17:12:37 +0900

    [2023-03-06]

--- src/main/java/io/springbatch/springbatchlecture/SpringBatchLectureApplication.java
+++ src/main/java/io/springbatch/springbatchlecture/SpringBatchLectureApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableBatchProcessing
+@EnableBatchProcessing //SpringBatch 활성화용
 public class SpringBatchLectureApplication {
 
     public static void main(String[] args) {
