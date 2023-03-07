57ea6268abc173418d85b41e7ef4585a1b8f958d
최광진 <38902367+arattha@users.noreply.github.com>
2023-03-07 16:27:34 +0900

    [2023-03-07]

--- src/main/java/io/springbatch/springbatchlecture/JobRunner.java
+++ src/main/java/io/springbatch/springbatchlecture/JobRunner.java
@@ -13,19 +13,26 @@ import java.util.Date;
 
 @Component
 public class JobRunner implements ApplicationRunner {
+    //ApplicationRunner -> Boot 가 초기화 되고 완료되면 실행
 
     @Autowired
-    private JobLauncher jobLauncher;
+    private JobLauncher jobLauncher;//초기화된 후라 의존성 주입 가능
 
     @Autowired
-    private Job job;
+    private Job job;//JobInstanceConf 의 Job 주입
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        //기본적으로 SpringBoot 가 실행시키는 방식과 유사
+        //이 경우 생성한 Job 이 Boot 가 실행하지 않도록 설정필요
+
+
 
         JobParameters jobParameters = new JobParametersBuilder()
-                .addString("name", "user1")
-//                .addDate("reqDate", new Date())
+                .addString("name", "user2")
+                // A job instance already exists and is complete for parameters={name=user1}.
+                // 2번 실행했을 때 나타나는 오류, 이미 있던 Instance 이기 때문
+//               .addDate("reqDate", new Date())
                 .toJobParameters();
 
         jobLauncher.run(job,jobParameters);
